3b2adcaa0c3dbf44de579d2e9e9a17989f68f6be
ZaydenKelly09 <zaydenkelly729@gmail.com>
2025-05-21 21:01:01 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,16 +2,238 @@
     const blogPosts = [
         {
             id: 1,
-            title: "My Main Tenets",
-            date: "April 22, 2025",
-            excerpt: "Learn the principles of effective dark mode design and how to implement it in your projects.",
+            title: "My Main Tenets - Entry #1",
+            date: "April 21, 2025",
+            excerpt: "",
             content: `
                 <p>
-                Hiiii!
+                After reading <i>Allies</i> I will continue to add to this journal for at least a month
+                and track my efforts on implementing the book's ideas on how to become a more effective
+                ally. The book repeatedly talks about how allyship is a dynamic journey of continuous learning
+                and action instead of a destination. Over the next month, I will focus on a few main tenets:
+                recognizing how allyship is ongoing and that mistakes are learning opportunities; engaging in
+                "Quiet Allyship" and using my voice to call in others and not to be a silent bystander; and the
+                important self-reflections of acknowledging my own privileges and biases.
+                </p>
+                <br>
+                <p>
+                I also want to have deeper empathy by trying to understand the inequalities that others face and
+                by asking myself how other people might be feeling during different situations. This involves challenging
+                narratives and understanding that my reality isn't everyone else's reality. In this journal I want to
+                document my experiences (both my successes and failures) applying these principles, while focusing on growth
+                and trying again.
+                </p>  
+                
+            `
+        },
+                {
+            id: 2,
+            title: "Entry #2",
+            date: "April 24, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                Today in Spanish class, during a conversation with some people I was sitting next to, including my best friend, 
+                they were saying that they "respected gay people but didn't respect what they were doing." This made me feel angry
+                and uncomfortable, as it felt like an invalidating kind of respect. While concepts from the book, like the importance 
+                of not being a silent bystander, came to mind, I stayed quiet. I did this mostly because I didn't want to lose one of 
+                my friends. Now that I look back on this, I feel like I missed an opportunity to be an ally and I prioritized my 
+                comfort. I now recognize this choice as a "screw up" in the journey the book emphasizes, which is full of learning 
+                and trying again; I want to try and find more courage to challenge things like this in the future.
                 </p>
                 
             `
         },
+                        {
+            id: 3,
+            title: "Entry #3",
+            date: "April 27, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                These past couple of days, while a few of my friends and I were talking about a new song by Chappell Roan, I found myself thinking about "Quiet 
+                Allyship," a concept discussed in the book. As we talked about how we liked her music and her support of different communities, 
+                I thought about how simply engaging with and promoting artists who are part of, or allies to, diverse communities can be a small but consistent 
+                act of support. It made me think about how my everyday choices in music or media can align with the ideas of allyship, even if I'm not actively 
+                thinking about it. It also made me reflect on how easily conversations about pop culture can either ignore or embrace opportunities to acknowledge 
+                diverse people.
+                </p>
+                
+            `
+        },
+                        {
+            id: 4,
+            title: "Entry #4",
+            date: "April 29, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                Today, I saw a really bad quote from a political candidate on X that talked about
+                how she was the only hope that Texas had to prevent the "Islamization" of America and she 
+                then talked about how she wouldn't trade the cross for the crescent. This post immediately
+                stood out to me as just plain wrong and Islamophobic, this directly contradicts the principles
+                of empathy and understanding that were talked about in the book. It made me think about how someone
+                in the book had bad experiences as a Muslim woman, and how statements like these create a bad environment.
+                I was really angry so I made a comment refuting the claim, backed up with solid reasoning and evidence.
+                This quote is a big reminder of the harmful ideas that exist and the need for allies to challenge these ideologies
+                and to not remain as silent bystanders.
+                </p>
+                
+            `
+        },
+                        {
+            id: 5,
+            title: "Entry #5",
+            date: "May 1, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                During a project in Health class these past couple of days, I noticed that one of my group
+                members' ideas were being shot down even if it was a good idea. It made me feel kind of angry 
+                and disappointed because their work wasn't being valued. It reminded me about how easily voices
+                can be quieter in group settings, I noticed this as a theme in many of the essays throughout the
+                book. I thought the advice about "passing the mic", making sure that everyone felt heard, and the 
+                importance of not just being a silent bystander. To make sure this person's voice was heard I tried
+                to shift the conversation toward them by asking what they were saying and that it sounded interesting.
+                Thinking about it now, that small thing seemed to shift the mood slightly, allowing them to share their
+                ideas. It also reinforces how allies can help create more inclusive spaces by making sure everyone has 
+                a chance to be heard.
+                </p>
+                
+            `
+        },
+                        {
+            id: 6,
+            title: "Entry #6",
+            date: "May 4, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                Today in Spanish class, as we were learning about the Bracero Program (agreement between the US
+                and Mexican government that let Mexican citizens do temporary farm work in the US) and I found myself
+                thinking about how some of the things we take for granted, like healthcare, they simply didn't have. This
+                made me reflect on my privileges, a big idea discussed in the book. It made me notice how much I take 
+                for granted the ability to go to the doctor whenever I feel sick, without a second thought about whether I could
+                afford it or even had access to it. This connects to the idea of recognizing "glitches" in our understanding of
+                the world, it made me consider the inequalities people face that impacts something as important as their health.
+                It also shows how much I still have to learn about the realities of marginalized groups and the importance of
+                empathy in learning about these groups.
+                </p>
+                
+            `
+        },
+                                {
+            id: 7,
+            title: "Entry #7",
+            date: "May 6, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                These past days, I've been really focused on self-reflection to change my language, specifically by not using the word
+                "retarded." I used to use this word a lot to comment on things that I found silly or foolish, but after reading the book
+                and thinking about the impact that words have, I realized how offensive and hurtful that word is. The "Some Great Word Swaps"
+                section and the assertion that "language matters" really stuck with me. Understanding that my use of the word could really hurt
+                someone's feelings, just like how I would feel if someone called me "fat" or "dumb," really cemented my decision to remove that
+                word from my everyday speech. This feels like a really good step in my allyship journey and really shows the personal accountability
+                and continuous learning that the book encourages.
+                </p>
+                
+            `
+        },
+                            {
+            id: 8,
+            title: "Entry #8",
+            date: "May 8, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                The election of a new American Pope today was celebrated across the nation, but I noticed
+                that most people just glossed over the fact that while he did grow up in Chicago he actually spent
+                most of his adult life in Peru. This focus on his "American-ness" kind of downplayed his great work in
+                Peru and made me think about how dominant stories can overshadow really big parts of someone's life, an
+                idea that was discussed throughout many of the essays in the book. It reminded me of how easily the complexities
+                of people can get forgotten when we try to fit them into boxes, and how certain parts of a person are highlighted
+                while other parts are ignored based on what fits in a particular story. We need to focus on a person's whole story,
+                not just the parts that are convenient for most people.
+                </p>
+                
+            `
+        },
+                                {
+            id: 9,
+            title: "Entry #9",
+            date: "May 11, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                Lately, I've been really annoyed by a specific thing with a friend who calls me "gay" as an insult when he is
+                frustrated or annoyed at me. This isn't about my real identity but rather his use of "gay" as an insult that bothers
+                me, especially after reading the book and thinking about the impact of language. It feels like the direct opposite of the respect and 
+                understanding that the book promotes, it is also a clear example of how simple phrases can create harm. I think this is a difficult position
+                because the concept of "calling in" a friend seems appropriate, but the fear of losing a friendship makes me really hesitate. This really shows
+                the challenge of balancing personal relationships and also my responsibility to speak out against language that insults anyone, and I am still trying
+                to think about how to deal with these uncomfortable but important discussions.
+                </p>
+                
+            `
+        },
+                                {
+            id: 10,
+            title: "Entry #10",
+            date: "May 13, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                These past couple of days, while watching a TV show with my parents, they made a racist remark and referred to some Muslim characters as "terrorists,"
+                a comment that immediately made me feel uncomfortable and angry. It was a big reminder of the bad stereotypes that Adiba Jaigirdar described facing as 
+                a Muslim person in the book and how language like that creates a hostile environment. Even though I thought about the importance of not being a silent bystander I
+                didn't say anything because I was worried about my parents' reaction if I challenged them. This highlighted a really difficult part of allyship for me: 
+                the trouble between wanting to challenge harmful ideas and also the fear of personal consequences. It also makes me think about how privilege and power, even in a family,
+                can silence someone. It shows that sometimes the safest option during the moment feels like you are doing nothing, even if it leaves me with regrets.
+                </p>
+                
+            `
+        },
+                                        {
+            id: 11,
+            title: "Entry #11",
+            date: "May 15, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                Today during lunch, a group of us were talking about the ambulance levy that is being voted on. One person commented something like, "I don't see why we need it,
+                we already have ambulances that work just fine," which felt like an oversimplification of the issue. Remembering the advice in the book about "calling in" 
+                while assuming that they were coming from a good place, I tried to gently challenge their statement by saying, "I get how it can seem like that but our ambulances can only provide
+                basic life support, when a person is really hurt that isn't enough and waiting for one to come from Rexburg can waste precious minutes." They seemed a bit defensive,
+                but it did shift the conversation toward a more thoughtful consideration rather than an outright dismissal. It was uncomfortable, but it felt like a step in encouraging empathy
+                and challenging perspectives, which was a big theme in the book.
+                </p>
+                
+            `
+        },
+                                        {
+            id: 12,
+            title: "Reflections - Entry #12",
+            date: "May 18, 2025",
+            excerpt: "",
+            content: `
+                <p>
+                As I write this final entry, looking back over the past month of engaging and learning from the book and tracking my progress, my biggest takeaway is that allyship really is a continuous
+                and usually uncomfortable journey of learning and self-reflection, just like the authors described. The tenets I initially focused on, like recognizing mistakes as learning opportunities,
+                using my voice in small ways, and acknowledging my privileges, have constantly been tested. I've learned that it's not about achieving a perfect state of being an ally, but about the consistent 
+                effort to show up, listen, and try, even when it's hard, like when I hesitated to speak up in Spanish or with my parents. The personal stories throughout the book have been really impactful, making the
+                principles of allyship feel more tangible and human.
+                <br>
+                Going forward, I hope to carry these skills into my daily life by being more mindful of my language, more observant about the power and privileges that are around me, and more brave in "calling in" hurtful
+                comments when I can. I also want to continue to learn and seek out many diverse perspectives, as the book promotes. I know I won't always be right, and there will be more screw ups, but this experience
+                has solidified my understanding that the commitment to learning from these moments and trying again is what truly matters. This journal has been a really good tool for processing these experiences, and I want to continue
+                the practice of reflection as I strive to be a more empathetic and effective ally.
+                </p>
+                
+            `
+        },
+        
+
     ];
 
     // Function to create post preview cards
